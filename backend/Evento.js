15ce1edfcd7630bf87e59e3a123c72fcd34000e8
Joan0880 <joancuevas2002@gmail.com>
2026-04-22 22:22:52 -0400

    Update Evento.js

--- backend/Evento.js
+++ backend/Evento.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const EventoSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
-  fecha: String,
-  disenoUrl: String, // Aquí irá el link de la imagen
-  descripcion: String
+  descripcion: String,
+  precio: { type: Number, required: true },
+  categoria: { type: String, enum: ['Sillas', 'Mesas', 'Mantelería', 'Lounge', 'Otros'] },
+  imagenUrl: String,
+  disponible: { type: Boolean, default: true },
+  tags: [String] // Ej: ["Boda", "Exterior", "Madera"]
 });
 
 module.exports = mongoose.model('Evento', EventoSchema);
